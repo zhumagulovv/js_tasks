e7eccb83201b7c27201082d8e0dd41c6bf954e82
Jakyp-05 <jakypjumagulov080@gmail.com>
2025-06-27 21:10:09 +0600

    Даны два целых числа. Найти остаток от деления первого числа на второе.

---
+++ 01.21_task/index.js
@@ -0,0 +1,10 @@
+// Даны два целых числа. Найти остаток от деления первого числа на второе.
+
+function getRemainder(a, b) {
+  return a % b;
+}
+
+console.log(getRemainder(20, 12)); // 8
+console.log(getRemainder(23, 12)); // 11
+console.log(getRemainder(10, 5)); // 0
+console.log(getRemainder(10, 2)); // 0
