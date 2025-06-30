c342a84cafc2a6981334ae50e7c1d3f2c0d7f285
Jakyp-05 <jakypjumagulov080@gmail.com>
2025-06-30 14:10:12 +0600

    // Даны два массива: Склеить эти массивы в новый массив

---
+++ 30-60.tasks/01.40_task/index.js
@@ -0,0 +1,11 @@
+// Даны два массива: Слейте эти массивы в новый массив
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+console.log(Array.from(new Set(arr1.concat(arr2))));
+
+console.log([...new Set([...arr1, ...arr2])]);
+
+const arr3 = arr1.concat(arr2);
+console.log(arr3);
