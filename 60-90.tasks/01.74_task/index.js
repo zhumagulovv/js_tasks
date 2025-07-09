234d39274a7ac429e986ba4de9a38057951f4d6c
Jakyp-05 <jakypjumagulov080@gmail.com>
2025-07-09 11:17:17 +0600

    Дан массив с числами. Выводить в консоль элементы массива в обратном порядке.

---
+++ 60-90.tasks/01.74_task/index.js
@@ -0,0 +1,7 @@
+// Дан массив с числами. Выводить в консоль элементы массива в обратном порядке.
+
+let numbers = [1, 2, 3, 4, 5];
+let reversed = numbers.reverse();
+
+console.log(arr);
+console.log(reversed);
