60635525be485fa802cf1d007595f4788afe7348
Jakyp-05 <jakypjumagulov080@gmail.com>
2025-07-13 14:51:14 +0600

    Дана некоторая переменная с числом. Надо сделать строку, содержащую столько нулей, сколько указано в переменной.

---
+++ 60-90.tasks/01.85_task/index.js
@@ -0,0 +1,12 @@
+// Дана некоторая переменная с числом. Надо сделать строку, содержащую столько нулей, сколько указано в переменной.
+
+let num = 5;
+let zeroStr = "0".repeat(num);
+
+console.log(zeroStr);
+
+let name = "Zhakyp ";
+
+let zeroName = name.repeat(10);
+
+console.log(zeroName);
